dfffb6de033dedbd092a2d4146bd61566eac37ef
nicolasdelbaer <n.delbaer@gmail.com>
2026-03-28 00:20:58 +0100

    refactor - update names from Participation

--- src/main/java/be/nidel/kinomichi/Participation.java
+++ src/main/java/be/nidel/kinomichi/Participation.java
@@ -1,5 +1,7 @@
 package be.nidel.kinomichi;
 
+import be.nidel.kinomichi.gathering.Gathering;
 import be.nidel.kinomichi.participant.Participant;
+import be.nidel.kinomichi.session.Session;
 
-public record Participation(Participant participant, Animation animation, Period period) {}
+public record Participation(Participant participant, Gathering gathering, Session session) {}
