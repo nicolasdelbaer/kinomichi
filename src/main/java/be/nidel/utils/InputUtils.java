82aeb7c6a11bbd33571d0fda8b9843a2c2fada94
nicolasdelbaer <n.delbaer@gmail.com>
2026-03-31 14:19:18 +0200

    refactor - input utils as static

---
+++ src/main/java/be/nidel/utils/InputUtils.java
@@ -0,0 +1,58 @@
+package be.nidel.utils;
+
+import java.math.BigDecimal;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class InputUtils {
+
+    public static LocalTime askTime(Scanner scanner, String s) {
+        LocalTime time = null;
+        do{
+            try{
+                time = DateUtils.StringTimeToLocalTime(askInput(scanner, s));
+            } catch (IllegalArgumentException | NullPointerException | DateTimeException ignored) {}
+        }while(Objects.isNull(time));
+        return time;
+    }
+
+    public static LocalDate askDate(Scanner scanner, String s) {
+        LocalDate date = null;
+        do{
+            try{
+                date = DateUtils.StringDateToLocalDate(askInput(scanner, s));
+            } catch (IllegalArgumentException | NullPointerException | DateTimeException ignored) {}
+        }while(Objects.isNull(date));
+        return date;
+    }
+
+    public static int askInt(Scanner scanner, String inputRequest) {
+        Integer result = null;
+        do{
+            try{
+                result = Integer.parseInt(askInput(scanner, inputRequest));
+            } catch (IllegalArgumentException | NullPointerException ignored) {}
+        }while(Objects.isNull(result));
+
+        return result;
+    }
+
+    public static BigDecimal askBigDecimal(Scanner scanner, String inputRequest) {
+        BigDecimal result = null;
+        do{
+            try{
+                result = new BigDecimal(askInput(scanner, inputRequest));
+            } catch (IllegalArgumentException|NullPointerException ignored) {}
+        }while(Objects.isNull(result));
+
+        return result;
+    }
+
+    public static String askInput(Scanner scanner, String inputRequest) {
+        OutputUtils.sOutInfo(inputRequest);
+        return scanner.nextLine();
+    }
+}
