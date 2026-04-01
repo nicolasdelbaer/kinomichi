4959fd2ebcf19b1918ab70eace6081b8a5e19bef
nicolasdelbaer <n.delbaer@gmail.com>
2026-04-01 15:53:52 +0200

    update - inline yes/no, back/quit inputs for menu

--- src/main/java/be/nidel/utils/menu/MenuFactory.java
+++ src/main/java/be/nidel/utils/menu/MenuFactory.java
@@ -1,5 +1,6 @@
 package be.nidel.utils.menu;
 
+import be.nidel.utils.OutputUtils;
 import be.technifutur.shared.Menu;
 
 //Objectif enlever de la complexité dans la création et gestion des menus
@@ -8,17 +9,19 @@ public class MenuFactory {
     public static MenuController backQuitTemplate(Menu context){
         Menu menu = new Menu();
         MenuController menuController = new MenuController(menu, context);
-        menu.addItem("back", "b", menuController::handleBack);
-        menu.addItem("quit", "q", menuController::handleQuit);
+        menu.addHiddenItem("back", "b", menuController::handleBack);
+        menu.addHiddenItem("quit", "q", menuController::handleQuit);
+        menu.setPostRender(OutputUtils.STYLISABLE_LINE.formatted(OutputUtils.ANSI_BLACK_BACKGROUND, "\"b\": back - \"q\": quit", OutputUtils.ANSI_RESET));
         return menuController;
     }
 
     public static MenuController confirmTemplate(Menu context, Runnable continueAction){
         Menu menu = new Menu();
         MenuController menuController = new MenuController(menu, context);
-        menu.addItem("yes", "y", continueAction);
-        menu.addItem("no", "n", menuController::handleBack);
-        menuController.setInteractionMessage("Continue doing this action? (y/n)");
+        menu.addHiddenItem("yes", "y", continueAction);
+        menu.addHiddenItem("no", "n", menuController::handleBack);
+        menu.setPostRender(OutputUtils.STYLISABLE_LINE.formatted(OutputUtils.ANSI_BLACK_BACKGROUND, "\"y\": yes - \"n\": no", OutputUtils.ANSI_RESET));
+        menuController.setInteractionMessage("Do you confirm?");
         return menuController;
     }
 }
